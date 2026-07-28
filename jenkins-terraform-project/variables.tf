be92604167233e915930d2597dcf02ecda17ecdc
Gaurav <117330941+sonawneg123@users.noreply.github.com>
2026-07-28 18:12:45 +0530

    Added Jenkins Terraform CI/CD project

---
+++ jenkins-terraform-project/variables.tf
@@ -0,0 +1,16 @@
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "EC2 Name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+}
+
